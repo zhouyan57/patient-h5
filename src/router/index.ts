d17999b0e11cadaa199d1deefd08c0e85ddce46f
Jane <994824764@qq.com>
2023-06-17 11:06:48 +0800

    开发准备 - 路由代码解析

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,14 @@
+// 导入函数
+//  createRouter：创建路由实例
+//  createWebHistory：创建 history 路由模式  localhost:3000/login
+//  createWebHashHistory：创建 hash 路由模式 localhost:3000/#/login
 import { createRouter, createWebHistory } from 'vue-router'
 
+// 创建路由对象
 const router = createRouter({
+  // 设置路由模式(history模式)
+  //  import.meta.env.BASE_URL：会去 vite.config.ts 中去取出配置项 base 中的值
+  //  将来请求页面路径时，会带上这个 base 路径（项目的基础路径前缀，默认是 /）
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
 })
