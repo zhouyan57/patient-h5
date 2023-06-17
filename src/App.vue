1f714243046de476351bf3d7469352c9eb8bc476
Jane <994824764@qq.com>
2023-06-17 11:24:31 +0800

    开发准备 - 集成 vant 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button as VanButton } from 'vant'
+</script>
 <template>
   <h2>根组件</h2>
+  <van-button type="primary">主要按钮</van-button>
 </template>
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import './styles/main.scss'
+// 导入 vant 样式
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 
