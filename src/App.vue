f0819b5c07c07a4bca441fadc375d6581caadf9d
Jane <994824764@qq.com>
2023-06-17 11:36:14 +0800

    开发准备 - 移动端适配

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,13 @@ import { Button as VanButton } from 'vant'
 <template>
   <h2>根组件</h2>
   <van-button type="primary">主要按钮</van-button>
+  <div class="box"></div>
 </template>
-<style scoped></style>
+<style scoped>
+.box {
+  margin-top: 20px;
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
+</style>
