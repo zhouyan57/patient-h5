2ea5e9c527fa3c75b0038319f607870b51b43ef4
Jane <994824764@qq.com>
2023-06-19 09:46:41 +0800

    登录模块 - 页面布局 - 头部&底部结构

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,8 +7,60 @@ const toRegister = () => {
 
 <template>
   <div class="login-page">
-    <cp-nav-bar right-text="注册" @click-right="toRegister"></cp-nav-bar>
+    <!-- 头部组件 -->
+    <cp-nav-bar :fixed="true" right-text="注册" @click-right="toRegister"></cp-nav-bar>
+    <!-- 头部区域 -->
+    <div class="login-head">
+      <h3>密码登录</h3>
+      <a href="javascript:;">
+        <span>短信验证码登录</span>
+        <van-icon name="arrow"></van-icon>
+      </a>
+    </div>
+    <!-- 表单区域 -->
+    <!-- 底部区域 -->
+    <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <div class="icon">
+        <img src="@/assets/qq.svg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+.login {
+  &-page {
+    padding-top: 46px;
+  }
+  &-head {
+    background-color: pink;
+    padding: 30px 30px 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    line-height: 1;
+    h3 {
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+    }
+  }
+
+  &-other {
+    margin-top: 60px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+</style>
