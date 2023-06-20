c1edd311a20c817953aa146755f24a9c5d0204a9
Jane <994824764@qq.com>
2023-06-20 13:51:29 +0800

    个人中心 - 设置用户信息类型(Pick和Omit用法)

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -12,3 +12,18 @@ export type User = {
 // login登录register注册changeMobile更换手机号forgetPassword找回密码,bindMobile绑定三方登录，区分验证码
 // 验证码的类型
 export type CodeType = 'login' | 'register' | 'changeMobile' | 'forgetPassword' | 'bindMobile'
+// 得到与 user 部分属性相同的类型
+type OmitUser = Omit<User, 'token' | 'refreshToken'>
+// 定义返回的用户对象类型
+export type UserInfo = OmitUser & {
+  likeNumber: number
+  collectionNumber: number
+  score: number
+  couponNumber: number
+  orderInfo: {
+    paidNumber: number
+    receivedNumber: number
+    shippedNumber: number
+    finishedNumber: number
+  }
+}
