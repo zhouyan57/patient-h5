e39e2b0522c9f1af69ec5b95fc05e680586aeeb7
Jane <994824764@qq.com>
2023-06-21 14:39:58 +0800

    v-model的用法 多个数据双向绑定

--- src/views/Notify/index.vue
+++ src/views/Notify/index.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import son from './son.vue'
 const num = ref(0)
+const msg = ref('f')
 </script>
 <template>
   <!-- <div class="notify-page">notify</div> -->
@@ -9,8 +10,10 @@ const num = ref(0)
   <h3>1. v-model 的用法</h3>
   <h3>父组件</h3>
   <div>num:{{ num }}</div>
-  <son v-model="num"></son>
+  <div>msg:{{ msg }}</div>
+  <son v-model="num" v-model:msg="msg"></son>
   <!-- 等价于以下写法 -->
   <!-- <son :modelValue="num" @update:modelValue="num = $event"></son> -->
+  <!-- <son v-model="num" :msg="msg" @update:msg="msg = $event"></son> -->
 </template>
 <style scoped lang="scss"></style>
--- src/views/Notify/son.vue
+++ src/views/Notify/son.vue
@@ -2,17 +2,24 @@
 // 接收 v-model 传过来的参数
 const props = defineProps<{
   modelValue: number
+  msg: string
 }>()
 const emits = defineEmits<{
   (e: 'update:modelValue', v: number): void
+  (e: 'update:msg', v: string): void
 }>()
 const addValue = () => {
   emits('update:modelValue', props.modelValue + 1)
+}
+const changeMsg = () => {
+  emits('update:msg', 'son')
 }
 </script>
 <template>
   <h3>子组件</h3>
   <div>modelValue: {{ modelValue }}</div>
+  <div>msg:{{ msg }}</div>
   <button @click="addValue">点我数字++</button>
+  <button @click="changeMsg">点我修改 msg</button>
 </template>
 <style scoped></style>
