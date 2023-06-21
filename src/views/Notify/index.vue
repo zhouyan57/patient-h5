2d4346b43e0ea5fb420d5600e7a8ed7c5b6b4ddc
Jane <994824764@qq.com>
2023-06-21 14:32:59 +0800

    vue3中v-model的用法

--- src/views/Notify/index.vue
+++ src/views/Notify/index.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import son from './son.vue'
+const num = ref(0)
+</script>
 <template>
-  <div class="notify-page">notify</div>
+  <!-- <div class="notify-page">notify</div> -->
+  <h2>vue3中的 v-model</h2>
+  <h3>1. v-model 的用法</h3>
+  <h3>父组件</h3>
+  <div>num:{{ num }}</div>
+  <son v-model="num"></son>
+  <!-- 等价于以下写法 -->
+  <!-- <son :modelValue="num" @update:modelValue="num = $event"></son> -->
 </template>
 <style scoped lang="scss"></style>
---
+++ src/views/Notify/son.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// 接收 v-model 传过来的参数
+const props = defineProps<{
+  modelValue: number
+}>()
+const emits = defineEmits<{
+  (e: 'update:modelValue', v: number): void
+}>()
+const addValue = () => {
+  emits('update:modelValue', props.modelValue + 1)
+}
+</script>
+<template>
+  <h3>子组件</h3>
+  <div>modelValue: {{ modelValue }}</div>
+  <button @click="addValue">点我数字++</button>
+</template>
+<style scoped></style>
