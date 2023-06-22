e8026d98ff088417174b36aac22b5b2cb1aa3756
Jane <994824764@qq.com>
2023-06-22 08:21:26 +0800

    首页模块 - 基础结构 - 导航区域

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,64 @@
 <script setup lang="ts"></script>
 <template>
   <div class="home-page">
+    <!-- 头部区域 -->
     <div class="home-header">
       <div class="con">
         <h1>优医</h1>
         <div class="search"><cp-icon name="home-search" /> 搜一搜：疾病/症状/医生/健康知识</div>
       </div>
+    </div>
+    <!-- 导航区域 -->
+    <div class="home-navs">
+      <van-row>
+        <van-col span="8">
+          <router-link to="/" class="nav">
+            <cp-icon name="home-doctor"></cp-icon>
+            <p class="title">问医生</p>
+            <p class="desc">按科室查问医生</p>
+          </router-link>
+        </van-col>
+        <van-col span="8">
+          <router-link to="/consult/fast" class="nav">
+            <cp-icon name="home-graphic"></cp-icon>
+            <p class="title">极速问诊</p>
+            <p class="desc">20s医生极速回复</p>
+          </router-link>
+        </van-col>
+        <van-col span="8">
+          <router-link to="/" class="nav">
+            <cp-icon name="home-prescribe"></cp-icon>
+            <p class="title">开药门诊</p>
+            <p class="desc">线上买药更方便</p>
+          </router-link>
+        </van-col>
+      </van-row>
+      <van-row>
+        <van-col span="6">
+          <router-link to="/" class="nav min">
+            <cp-icon name="home-order"></cp-icon>
+            <p class="title">药品订单</p>
+          </router-link>
+        </van-col>
+        <van-col span="6">
+          <router-link to="/" class="nav min">
+            <cp-icon name="home-docs"></cp-icon>
+            <p class="title">健康档案</p>
+          </router-link>
+        </van-col>
+        <van-col span="6">
+          <router-link to="/" class="nav min">
+            <cp-icon name="home-rp"></cp-icon>
+            <p class="title">我的处方</p>
+          </router-link>
+        </van-col>
+        <van-col span="6">
+          <router-link to="/" class="nav min">
+            <cp-icon name="home-find"></cp-icon>
+            <p class="title">疾病查询</p>
+          </router-link>
+        </van-col>
+      </van-row>
     </div>
   </div>
 </template>
@@ -13,6 +66,7 @@
 .home-page {
   padding-bottom: 50px;
 }
+// 头部区域
 .home-header {
   height: 100px;
   position: relative;
@@ -51,6 +105,39 @@
       .cp-icon {
         font-size: 16px;
         margin-right: 5px;
+      }
+    }
+  }
+}
+// 导航区域
+.home-navs {
+  padding: 10px 15px 0 15px;
+  .nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 0;
+    .cp-icon {
+      font-size: 48px;
+    }
+    .title {
+      font-weight: 500;
+      margin-top: 5px;
+      color: var(--cp-text1);
+    }
+    .desc {
+      font-size: 11px;
+      color: var(--cp-tag);
+      margin-top: 2px;
+    }
+    &.min {
+      .cp-icon {
+        font-size: 31px;
+      }
+      .title {
+        font-size: 13px;
+        color: var(--cp-text2);
+        font-weight: normal;
       }
     }
   }
