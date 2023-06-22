dd0367e3093665e33cf1348d1f6c763bf42d2101
Jane <994824764@qq.com>
2023-06-22 08:14:36 +0800

    首页模块 - 基础结构 - 头部区域

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,58 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="home-page">home</div>
+  <div class="home-page">
+    <div class="home-header">
+      <div class="con">
+        <h1>优医</h1>
+        <div class="search"><cp-icon name="home-search" /> 搜一搜：疾病/症状/医生/健康知识</div>
+      </div>
+    </div>
+  </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-page {
+  padding-bottom: 50px;
+}
+.home-header {
+  height: 100px;
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 90px;
+    background: linear-gradient(180deg, rgba(62, 206, 197, 0.85), #26bcc6);
+    border-bottom-left-radius: 150px 20px;
+    border-bottom-right-radius: 150px 20px;
+  }
+  .con {
+    position: relative;
+    padding: 0 15px;
+    > h1 {
+      font-size: 18px;
+      color: #fff;
+      font-weight: normal;
+      padding: 20px 0;
+      line-height: 1;
+      padding-left: 5px;
+    }
+    .search {
+      height: 40px;
+      border-radius: 20px;
+      box-shadow: 0px 15px 22px -7px rgba(224, 236, 250, 0.8);
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      padding: 0 20px;
+      color: var(--cp-dark);
+      font-size: 13px;
+      .cp-icon {
+        font-size: 16px;
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
