915e3b6fe597673c3f9a60fc79be70e145a4fa5f
Jane <994824764@qq.com>
2023-06-26 22:32:57 +0800

    问诊室 - 获取默认信息

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -6,10 +6,14 @@ import io from 'socket.io-client'
 import { BASEURL } from '@/utils/request'
 import { useUserStore } from '@/stores'
 import { useRoute } from 'vue-router'
-import { onUnmounted } from 'vue'
+import { onUnmounted, ref } from 'vue'
+import type { Message, TimeMessages } from '@/types/room'
+import { MsgType } from '@/enums/index'
 // 1. 使用 websocket 来连接服务器
 const userStore = useUserStore()
 const route = useRoute()
+// 返回信息数据源
+const list = ref<Message[]>([])
 // 与服务端建立连接
 const socket = io(BASEURL, {
   auth: {
@@ -23,6 +27,23 @@ const socket = io(BASEURL, {
 // 建立连接成功的回调
 socket.on('connect', () => {
   console.log('连接成功')
+  // 2. 获取默认聊天记录
+  // 定义默认聊天记录数组
+  const defaultArr: Message[] = []
+  socket.on('chatMsgList', (res: { data: TimeMessages[] }) => {
+    // 得到返回消息的时间，生成为一条信息
+    defaultArr.push({
+      msgType: MsgType.Notify,
+      createTime: res.data[0].createTime,
+      msg: {
+        content: res.data[0].createTime
+      }
+    })
+    // 将剩余的消息添加到默认聊天记录中
+    defaultArr.push(...res.data[0].items)
+    // 将默认聊天记录添加到消息记录中
+    list.value.unshift(...defaultArr)
+  })
 })
 // 监听断开连接
 socket.on('disconnect', () => {
